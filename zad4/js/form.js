414bbb6d02ef1232116c719ed766c8c187bbe4a3
Magdalena Głażewska <madbutterfly.mail@gmail.com>
2018-09-13 20:50:31 +0200

    Final commit

--- zad4/js/form.js
+++ zad4/js/form.js
@@ -1,12 +1,43 @@
-function change (el) {
-    var max_len = 10;
-    if (el.value.length > max_len) {
-        el.value = el.value.substr(0, max_len);
-    }
-    document.getElementById('char_cnt').innerHTML = el.value.length;
-    document.getElementById('chars_left').innerHTML = max_len - el.value.length;
-    return true;
+//object with regular expressions
+var regexPatterns = {
+    name: /^[A-ZĆŁÓŚŹŻ][a-ząćęłóśźż]{1,30}$/,
+    surname: /^[A-ZĄĆĘŁÓŚŹŻ][a-ząćęłóśźż]{1,30}(-[A-ZĄĆĘŁÓŚŹŻ][a-ząćęłóśźż]{1,30})?$/,
+    email: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+};
+//getting form items
+var btnSend = document.getElementById("sub-button");
+var inputs = document.querySelectorAll("input");
+var agree = document.getElementById("agree");
+//setting event listeners
+for (var i = 0; i < inputs.length; i++){
+    inputs[i].addEventListener("change",validate);
+    inputs[i].addEventListener("blur",validate);
 }
-function postal(el) {
-    if(el.value.length===2 && el.value.indexOf('-')===-1) this.value+='-';
+
+agree.addEventListener("change", validate);
+//validation functions
+function validateNonTextInputs(element){
+    var result = true;
+    if(!agree.checked){
+        if(agree === element) agree.parentNode.style ="color: #ff0000";
+        result = false;
+    }
+    else agree.parentNode.style = "";
+    return result;
+}
+function validate(){
+    var isValid = true;
+    for(var i = 0; i < inputs.length; i++){
+        console.log(regexPatterns[inputs[i].id]);
+        if(!regexPatterns[inputs[i].id].test(inputs[i].value) && inputs[i].type.toLowerCase() === "text") {
+            isValid = false;
+            if(inputs[i] === this) this.style = "border: 2px #ff0000 solid";
+        }
+        else inputs[i].style ="";
+    }
+    var nonTextValid = validateNonTextInputs(this);
+    if(isValid && nonTextValid){
+        btnSend.disabled = false;
+    }
+    else btnSend.disabled = true;
 }
